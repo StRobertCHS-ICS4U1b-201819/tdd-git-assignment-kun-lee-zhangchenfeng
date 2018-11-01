73d8c456339df72c5e79bd817094b839a95bf3f2
zhang11111111 <box.zhang01@gmail.com>
2018-11-01 17:21:42 -0400

    working code for variance- comments later on

--- statTools.py
+++ statTools.py
@@ -17,6 +17,11 @@ def lower_quart():0
 
 def upper_quart():0
 
-def variance():0
+
+def variance(data):
+    if len(data) == 0:return 0
+    mean = sum(data) / len(data)
+    return round(sum([(item - mean) ** 2 for item in data]) / len(data), 4)
+
 
 def standard_dev():0
