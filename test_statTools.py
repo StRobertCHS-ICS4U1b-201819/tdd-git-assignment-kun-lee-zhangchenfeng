840823a152284e61a960456f0a802d23aad29c72
zhang11111111 <box.zhang01@gmail.com>
2018-11-01 17:21:11 -0400

    added test case

--- test_statTools.py
+++ test_statTools.py
@@ -23,9 +23,11 @@ def test_upper_quart():0
 
 def test_variance():
     assert(variance([0]) == 0)
-    assert(variance([16, 20, 3, 22, 24, 8]) == 57.92)
+    assert(variance([16, 20, 3, 22, 24, 8]) == 57.9167)
     assert(variance([]) == 0)
-    assert(variance([6]) == 6)
+    assert(variance([6]) == 0)
+    assert(variance([-34, -102, -82, 172, 140, -80, 167, -19, 6, 99, 132, -154, -200, 139, 53, 198, 173, -66, 87, -15, 1, 143, 83, -106, 167, -81, -1, -140, 47, -141, 42, -75, -41, 9, 123, 75, 9, 125, -173, 71, -107, 69, -184, 103, 162, -53, -170, 161, 51, -180]
+) == 13281.6564)
 
 
 def test_standard_dev():0
