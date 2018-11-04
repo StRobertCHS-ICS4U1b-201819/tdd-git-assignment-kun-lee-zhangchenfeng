f7d68fa563634b9e07e52769516176592a5c75a0
zhang11111111 <box.zhang01@gmail.com>
2018-11-04 10:34:07 -0500

    add test 8

--- test_statTools.py
+++ test_statTools.py
@@ -16,12 +16,28 @@ def test_mode():0
 
 def test_rng():0
 
-def test_lower_quart():0
+
+def test_lower_quart():
+    assert(lower_quartile([0]) == None)
+    # parameter and return
+    assert(lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9]) == 2.5000)
+    # odd number of items in list- each half has even number of items
+    assert(lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) == 3)
+    # odd number of items in list- each half has odd number of items
+    assert(lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 3)
+    # even number of items in list- each half has odd number of items
+    assert(lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) == 3.5000)
+    # even number of items in list- each half has even number of items
+    assert(lower_quartile([]) is None)
+    # empty list
+    assert(lower_quartile([1, 2, 3]) is None)
+    # less than 4 items in list
+    assert(lower_quartile([21, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1]) == 3.5)
+    # unsorted list
+
 
 def test_upper_quart():0
 
 def test_variance():0
 
 def test_std():0
-
-
