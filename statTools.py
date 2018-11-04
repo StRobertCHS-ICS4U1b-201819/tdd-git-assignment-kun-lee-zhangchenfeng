5c5c47deefc44aa40a77ee992483f6095e7ad90c
zhang11111111 <box.zhang01@gmail.com>
2018-11-04 10:23:09 -0500

    added docstring made code easier to read

--- statTools.py
+++ statTools.py
@@ -15,11 +15,20 @@ def rng():0
 
 
 def lower_quartile(data):
+    """
+    Returns the lower quartile of a list of integers
+
+    :param data: list of Integers
+    :return: float rounded to 4 decimal the lower quartile of the data. None if no lower quartile
+    """
     length = len(data)
-    if length<4:return None
+    if length < 4:
+        return None
     list.sort(data)
-    if length%4in[2, 3]:raw_output = data[length//4]
-    else:raw_output = (data[length//4] + data[length//4-1])/2
+    if length % 4 in [2, 3]:
+        raw_output = data[length // 4]
+    else:
+        raw_output = (data[length // 4] + data[length // 4 - 1]) / 2
     return round(raw_output, 4)
 
 
